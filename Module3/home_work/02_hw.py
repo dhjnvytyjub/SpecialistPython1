c087f50aa4ea4eb88ea6da621404e909f32b141f
Nisskiy <80744633+dhjnvytyjub@users.noreply.github.com>
2021-09-21 15:33:08 +0300

    Update 02_hw.py

--- Module3/home_work/02_hw.py
+++ Module3/home_work/02_hw.py
@@ -1,8 +1,5 @@
-# Напишите алгоритм, заполняющий список произвольными целыми числами
-# в диапазоне от -100 до 100. В списке должно быть n - элементов.
-# Подсказка:
-# для получения случайного числа используйте функцию randint() модуля random
-import random
-numbers = []
-# print(random.randint(10, 20))  # раскомментируйте, чтобы посмотреть работу функции randint
-# TODO: your code here
+from random import randint
+n = []
+for _ in range(int(input())):
+    n.append(randint(-100, 100))
+print(n)
