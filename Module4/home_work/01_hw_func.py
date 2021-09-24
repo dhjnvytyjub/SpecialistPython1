3174a89b050e752b896aa49b5f78db0b2e993976
Nisskiy <80744633+dhjnvytyjub@users.noreply.github.com>
2021-09-24 13:50:02 +0300

    Update 01_hw_func.py

--- Module4/home_work/01_hw_func.py
+++ Module4/home_work/01_hw_func.py
@@ -1,13 +1,13 @@
-# Дан шестизначный номер билета. Определить, является ли билет счастливым.
-# Решение реализовать в виде функции.
-# Билет считается счастливым, если сумма его первых и последних цифр равны.
+def lucky_ticket(t_n):
+    if len(str(t_n)) == 6:
+        if t_n % 10 + t_n % 100 // 10 + t_n % 1000 // 100 == t_n % 10000 // 1000 + t_n % 100000 // 10000 + t_n % 1000000 // 100000:
+            return'Билет', t_n, 'счастливый'
+        else:
+            return 'Билет', t_n, 'несчастливый'
+    else:
+        return 'Билет',t_n, 'недействительный'
 
-def lucky_ticket(ticket_number):
-    # TODO: your code here
-    pass
 
-
-# Тестируем функцию
-print(lucky_ticket(123006))
-print(lucky_ticket(12321))
-print(lucky_ticket(436751))
+print(*lucky_ticket(123006))
+print(*lucky_ticket(12321))
+print(*lucky_ticket(436751))
