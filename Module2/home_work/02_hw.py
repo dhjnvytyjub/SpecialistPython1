38b3f1742b6d7d5b53988af685abbfbda12083fc
Nisskiy <80744633+dhjnvytyjub@users.noreply.github.com>
2021-09-26 11:29:46 +0300

    Update 02_hw.py

--- Module2/home_work/02_hw.py
+++ Module2/home_work/02_hw.py
@@ -1,5 +1,5 @@
 n = int(input())
-if n % 10 == 1 and n != 11:
+if n % 10 == 1 and n % 100 != 11:
     print(n, 'korova')
 elif (n % 10 == 2 and n != 12) or (n % 10 == 3 and n != 13) or (n % 10 == 4 and n != 14):
     print(n, 'korovy')
